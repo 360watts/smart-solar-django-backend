c6b2f02f67e8481a6273cf4dc22ff0b62567525d
Admin-360watts <hello@360watts.com>
2026-01-22 17:46:48 +0530

    Update SECRET_KEY to a more secure default value

--- localapi/settings.py
+++ localapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-default-key-for-vercel')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-1^-iir7!mnx80ofg=%ebz%dhrer^@vor%zoy99oit*n2j)p!ts')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
